a85b70b910700355a9bed634dd3a6403f341f050
vishal-vp <21176895+vishal-vp@users.noreply.github.com>
2019-08-24 13:13:57 +0530

    Implement Clock

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Digit from '../Digit';
 import './Clock.scss';
 
 
+function getTimeObject() {
+  const date = new Date();
+  const hours = ("0" + date.getHours()).slice(-2);
+  const minutes = ("0" + date.getMinutes()).slice(-2);
+  const seconds = ("0" + date.getSeconds()).slice(-2);
+  const time = { hours, minutes, seconds };
+  return time;
+}
+
+function TimeUnit(props) {
+  return (
+    <div className='time-unit'>
+      <Digit value={props.value[0]} disabled={props.disabled} />
+      <Digit value={props.value[1]} disabled={props.disabled} />
+    </div>
+  );
+}
+
 function Clock(props) {
+  const [time, setTime] = useState(getTimeObject());
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(getTimeObject());
+    }, 1000);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
   return (
     <div className='clock'>
-      <div className='time-unit'>
-        <Digit value={0} disabled={true} />
-        <Digit value={1} disabled={true} />
-      </div>
-      <div className='time-unit'>
-        <Digit value={2} disabled={true} />
-        <Digit value={4} disabled={true} />
-      </div>
-      <div className='time-unit'>
-        <Digit value={3} disabled={true} />
-        <Digit value={9} disabled={true} />
-      </div>
+      <TimeUnit disabled={true} value={time.hours} />
+      <TimeUnit disabled={true} value={time.minutes} />
+      <TimeUnit disabled={true} value={time.seconds} />
     </div>
   );
 }
