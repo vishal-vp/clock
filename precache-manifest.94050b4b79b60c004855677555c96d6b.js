63e8f8604266da345fb286507103e7df616c18ab
vishal-vp <21176895+vishal-vp@users.noreply.github.com>
2019-08-29 22:37:34 +0530

    Updates

---
+++ precache-manifest.94050b4b79b60c004855677555c96d6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ade058f39718acb51a53c156b42a27f",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "3059e74be23e770a9653",
+    "url": "/clock/static/css/main.e1c268a9.chunk.css"
+  },
+  {
+    "revision": "09c17e5087eb88ca05b7",
+    "url": "/clock/static/js/2.8eeefa5a.chunk.js"
+  },
+  {
+    "revision": "3059e74be23e770a9653",
+    "url": "/clock/static/js/main.c3645322.chunk.js"
+  },
+  {
+    "revision": "56db40863b64fdb051d2",
+    "url": "/clock/static/js/runtime~main.e4563aac.js"
+  },
+  {
+    "revision": "bca467fd99729e72f2785f4d46bb1931",
+    "url": "/clock/static/media/day.bca467fd.svg"
+  },
+  {
+    "revision": "1f2142ef4229823c37beee563e185e28",
+    "url": "/clock/static/media/night.1f2142ef.svg"
+  }
+]);
